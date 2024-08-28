20fc8c808ad8e21e29c5fe8acd9436080f4274a1
habi <xh17671043325@163.com>
2024-08-28 15:42:44 +0800

    登录接口完善以及颁发token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,8 @@
-const { createUser } = require("../service/user.service")
-const { userRegisterError } = require("../constant/err.type")
+const jwt = require("jsonwebtoken")
+
+const { createUser, getUserInfo } = require("../service/user.service")
+const { userRegisterError, userLoginError } = require("../constant/err.type")
+const { JWT_SECRET } = require("../config/config.default")
 
 class UserController {
   async register(ctx) {
@@ -21,8 +24,21 @@ class UserController {
   }
 
   async login(ctx) {
-    const { username, password } = ctx.request.body
-    ctx.body = `${username}登录成功`
+    const { username } = ctx.request.body
+    try {
+      const { password, ...res } = await getUserInfo({ username })
+
+      ctx.body = {
+        code: 0,
+        success: true,
+        message: "用户登录成功",
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      }
+    } catch (e) {
+      return ctx.app.emit("error", userLoginError, ctx)
+    }
   }
 }
 
